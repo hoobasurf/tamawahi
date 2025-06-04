2d9b1f21e45bdf73800c9790dc5b472140c5e892
hoobasurf <brice.paneela@icloud.com>
2025-06-04 19:33:55 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -38,3 +38,28 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+const CACHE_NAME = 'tamawahi-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/assets/icons/lilo-stitch-icon-192.png',
+  '/assets/icons/lilo-stitch-icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
